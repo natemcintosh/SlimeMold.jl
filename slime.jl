66a7bbf465d7535b5ee63ddc686ba2a11956c5ce
Nate McIntosh <natemcintosh@gmail.com>
2025-01-20 22:51:12 -0500

    a little more progress

--- slime.jl
+++ slime.jl
@@ -10,7 +10,7 @@ end
 function slime_step!(agent, model)
     # Based on the agent's current heading, pick a position in that direction
     # with some amount of randomness
-    new_heading = agent.heading_rad + randn()
+    new_heading = agent.heading_rad + randn() * 0.1
 
     # Update the agent's heading
     agent.heading_rad = new_heading
@@ -20,6 +20,21 @@ function slime_step!(agent, model)
 
     # Have the agent walk in the new direction
     walk!(agent, direction, model)
+end
+
+# This is the step in which the particles deposit a pheromone in their current location,
+# the pheromone diffuses, then decays.
+function ground_step!(model)
+    # === Pheromone deposition =================================================
+    # For each neighbor
+    for agent in allagents(model)
+        # Deposit pheromone
+        model.ground[agent.pos[1], agent.pos[2]] += 1
+    end
+
+    # === Pheromone diffusion ==================================================
+
+    # === Pheromone decay ======================================================
 end
 
 function initialize(;
@@ -32,10 +47,11 @@ function initialize(;
     model = StandardABM(
         Slime,
         space;
-        # properties = properties,
+        properties = (ground = zeros(Float64, gridsize),),
         rng = rng,
         container = Vector,
         agent_step! = slime_step!,
+        model_step! = ground_step!,
     )
 
     # Populate the model with agents
@@ -46,7 +62,7 @@ function initialize(;
     model
 end
 
-function run_model!(model, n_steps::Int = 10)
+function run_model!(model; n_steps::Int = 10)
     agent_data = [:pos]
     adf, mdf = run!(model, n_steps; adata = agent_data)
     adf
@@ -57,9 +73,11 @@ function animate(; seed = 42)
     abmvideo(
         "slime.mp4",
         model;
-        frames = 500,
+        frames = 10,
         title = "Slime mold simulation",
         agent_size = 5,
-        agent_color = :black,
+        agent_color = :green,
+        # How do I make the background white?
+        # agentsplotkwargs = (backgroundcolor = :white),
     )
 end
